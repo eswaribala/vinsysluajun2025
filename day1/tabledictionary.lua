041e5e6872ec66277d152087e741b61156b6c365
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-06-27 12:47:08 +0530

    updated

--- day1/tabledictionary.lua
+++ day1/tabledictionary.lua
@@ -17,10 +17,27 @@ for _ =1,count do
         host = faker:city(),
         port = math.random(1000,9999),
         username = faker:state(),
-        password = faker:surname()
+        password = faker:surname(),
+        validate=function ()
+            local status=false
+            if #dbconfig["password"] >= 8
+                and dbconfig.password:match("%l")      -- lowercase
+                and dbconfig.password:match("%u")      -- uppercase
+                and dbconfig.password:match("%d")      -- digit
+                and dbconfig.password:match("[%p%s]") then
+                status=true
+            end
+            return status
+        end
     }
     table.insert(dbconfigurations,dbconfig)
 end
+
+--sorting db configurations by name
+table.sort(dbconfigurations, function(dbconfiguration1, dbconfiguration2)
+    return dbconfiguration1.port < dbconfiguration2.port
+end)
+
 -- iterate over db configurations
 for index,dbconfig_value in ipairs(dbconfigurations) do
     print(string.format("DB Configuration %d:", index))
@@ -29,4 +46,5 @@ for index,dbconfig_value in ipairs(dbconfigurations) do
     print(string.format("  Port: %d", dbconfig_value.port))
     print(string.format("  Username: %s", dbconfig_value.username))
     print(string.format("  Password: %s", dbconfig_value.password))
+    print(string.format("  Validate: %s", dbconfig_value.validate()))
 end
