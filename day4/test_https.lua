8644835673d149a14e5cf5fb66397b4b2c22bc0f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-06-30 12:16:03 +0530

    updated

--- day4/test_https.lua
+++ day4/test_https.lua
@@ -34,9 +34,38 @@ print("Status:", status)
 print("Response Body:", table.concat(responseBody))
 print("Response Code:", code)
 print("Response Headers:", responseHeaders) 
-if code == 201 then
+if code == 200 then
   print("Post created successfully")
 else
   print("Failed to create post, status code:", code)
 end
 
+if responseHeaders then
+  for k, v in pairs(responseHeaders) do
+    print(k, v)
+  end
+else
+  print("No response headers received")
+end
+if status then
+  print("Response status:", status)
+else
+  print("No response status received")
+end
+
+-- Check if the response body is not empty
+if #responseBody > 0 then
+  -- Decode the JSON response
+  local jsonResponse, pos, err = dkjson.decode(table.concat(responseBody), 1, nil)
+  if err then
+    print("Error decoding JSON:", err)
+  else
+    -- Print the decoded JSON response
+    print("Decoded JSON Response:")
+    for k, v in pairs(jsonResponse) do
+      print(k, v)
+    end
+  end
+else
+  print("No response body received")
+end
