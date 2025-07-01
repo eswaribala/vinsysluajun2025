a3c2723b09e7a380fd3fd794839618b58ea90b06
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-01 12:38:27 +0530

    updated

---
+++ day5/pcallv1.lua
@@ -0,0 +1,19 @@
+
+function productivity(unit_consumption, production_value)
+    local success, result = pcall(function()
+       return unit_consumption / production_value
+    end)
+
+    if success then
+        return result  -- 'result' contains the productivity value
+    else
+        error("Error in productivity calculation: " .. result)  -- 'result' contains the error message
+
+    end
+end
+
+
+
+local unit_consumption=math.random(1, 1000)
+local production_value=math.random(0,1)
+print(productivity(unit_consumption, production_value))
